65f237cdd124ebe05fb8d7e6d3a0ed518f1c664e
calosguilherme <calosguilherme@hotmail.com>
2017-06-20 21:12:46 -0300

    Get /Set e atributos

--- src/projetopoo/Regra.java
+++ src/projetopoo/Regra.java
@@ -13,4 +13,28 @@ public class Regra {
     private String tipo;
     private int ladoD;
     private int ladoE;
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public int getLadoD() {
+        return ladoD;
+    }
+
+    public void setLadoD(int ladoD) {
+        this.ladoD = ladoD;
+    }
+
+    public int getLadoE() {
+        return ladoE;
+    }
+
+    public void setLadoE(int ladoE) {
+        this.ladoE = ladoE;
+    }
 }
