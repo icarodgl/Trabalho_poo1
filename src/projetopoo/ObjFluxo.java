1df51193e2cdc3e41a4256cc7d209a807a4183e9
calosguilherme <calosguilherme@hotmail.com>
2017-07-02 17:41:54 -0300

    Add files via upload

---
+++ src/projetopoo/ObjFluxo.java
@@ -0,0 +1,29 @@
+package projetopoo;
+
+
+import java.util.Collection;
+
+public class ObjFluxo extends Modelo {
+        public String tipo;
+	private String nome;
+	
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+	
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+ 
+        
+
+}
